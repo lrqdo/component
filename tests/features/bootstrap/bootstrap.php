7d532b671b2b1ff3608f3f054f818f3fd6c6b5c6
Halleck45 <jeanfrancois@lepine.pro>
2013-01-08 12:06:56 +0100

    Behat is initialized

---
+++ tests/features/bootstrap/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__.'/../../../vendor/autoload.php';
+spl_autoload_register(function($class) {
+    $file = __DIR__.'/../../src/'.preg_replace('![_\\\\]!', DIRECTORY_SEPARATOR, $class).'.php';
+
+    if(file_exists($file)) {
+        require_once $file;
+    }
+});
