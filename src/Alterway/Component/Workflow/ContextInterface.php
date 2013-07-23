c6702e6dba6558f0052d715456ef1e10b1a735b4
gh0stonio <antoine.leveque.perso@gmail.com>
2013-07-23 11:03:00 +0200

    update context into a parameterbag

--- src/Alterway/Component/Workflow/ContextInterface.php
+++ src/Alterway/Component/Workflow/ContextInterface.php
@@ -4,12 +4,39 @@
 namespace Alterway\Component\Workflow;
 
 
+use Symfony\Component\DependencyInjection\Exception\ParameterNotFoundException;
+
 interface ContextInterface
 {
     /**
-     * Return the context defined by the given name
+     * Adds parameters to the service container parameters.
      *
-     * @param $name
+     * @param array $parameters An array of parameters
+     *
+     * @api
+     */
+    public function add(array $parameters);
+
+    /**
+     * Gets a service container parameter.
+     *
+     * @param string $name The parameter name
+     *
+     * @return mixed  The parameter value
+     *
+     * @throws ParameterNotFoundException if the parameter is not defined
+     *
+     * @api
      */
     public function get($name);
+
+    /**
+     * Sets a service container parameter.
+     *
+     * @param string $name  The parameter name
+     * @param mixed $value The parameter value
+     *
+     * @api
+     */
+    public function set($name, $value);
 }
